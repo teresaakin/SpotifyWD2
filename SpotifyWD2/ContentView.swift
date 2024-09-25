e18858641847775fd0d37ae9f3b53df11a04d6ba
Teresa <akinbodt@mcmaster.ca>
2024-09-25 15:01:16 -0400

    foundation for carousel widget

--- SpotifyWD2/ContentView.swift
+++ SpotifyWD2/ContentView.swift
@@ -2,10 +2,11 @@
 //  ContentView.swift
 //  SpotifyWD2
 //
-//  Created by Teresa Akinbodun on 2024-09-25.
+//  Created by Teresa Akinbodun
 //
 
 import SwiftUI
+
 
 struct ContentView: View {
     var body: some View {
@@ -13,12 +14,69 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer().frame(height: 8) // Set a fixed height for spacing
+            Text("Hello, BC NY!")
         }
         .padding()
     }
 }
 
+struct ColorItem {
+    let name: String
+    let color: Color
+}
+
+struct spotifyWidget: View {
+    
+    
+    
+    let colors: [ColorItem] = [
+        ColorItem(name: "Yugi", color: .blue),
+        ColorItem(name: "Kaiba", color: .red),
+        ColorItem(name: "Joey", color: .yellow),
+
+
+    ]
+    
+    
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.black)
+                .frame(width: 155, height: 155)
+                .overlay(
+                    GeometryReader { geometry in
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 20) {
+                                ForEach(colors, id: \.name) { item in
+                                    VStack {
+                                        Circle()
+                                            .fill(item.color)
+                                             .frame(width: 60, height: 60)
+                                             .padding(.top, 77)
+                                         Text(item.name)
+                                             .font(.caption)
+                                             .foregroundColor(.white)
+                                         
+                                    }
+ 
+                                }
+                            }
+                            .padding(.horizontal, (geometry.size.width - 60 * CGFloat(colors.count) - 20 * CGFloat(colors.count - 1)) / 2) // Centering padding
+
+                        }
+                        .frame(height: 77)
+                        
+                    }
+                )
+                .padding()
+
+        }
+    }
+}
+
+
 #Preview {
     ContentView()
+    spotifyWidget()
 }

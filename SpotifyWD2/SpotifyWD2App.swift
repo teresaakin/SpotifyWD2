a034364944e149e770d00965e8120d87fcd8a2ac
Teresa <akinbodt@mcmaster.ca>
2024-09-25 13:29:45 -0400

    Initial Commit

---
+++ SpotifyWD2/SpotifyWD2App.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyWD2App.swift
+//  SpotifyWD2
+//
+//  Created by Teresa Akinbodun on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyWD2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
